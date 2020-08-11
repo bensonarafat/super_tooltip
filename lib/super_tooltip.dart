c0641200211ab565131cb39a82b9ccbe9b609599
Christophe Bonello <cbonello@users.noreply.github.com>
2020-08-11 11:31:21 -0400

    fix closed-button offset relative to the top of the bubble shape

--- lib/super_tooltip.dart
+++ lib/super_tooltip.dart
@@ -983,7 +983,7 @@ EdgeInsets _getTooltipMargin({
   @required PreferredDirection preferredDirection,
 }) {
   final top =
-      (showCloseButton == ShowCloseButton.outside) ? closeButtonSize + 5 : 0.0;
+      (showCloseButton == ShowCloseButton.outside) ? closeButtonSize + 12 : 0.0;
 
   switch (preferredDirection) {
     case PreferredDirection.down:
