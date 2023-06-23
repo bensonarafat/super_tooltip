b5eea391e366b8086df739859a405e8f13a1c59f
Benson Arafat <benpaul320@gmail.com>
2023-06-23 18:43:38 +0100

    fix: Update Readme

--- index.js
+++
@@ -1,6 +0,0 @@
-
-const moment = require('moment');
-const simpleGit = require('simple-git');
-
-const DATE = moment().subtract(7.5, 'months').format();
-simpleGit().add("./*").commit("Update TD", {'--date': DATE}).push();
