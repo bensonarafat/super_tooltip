0fb41b0023a2a45c18f90b5ab4ff74047e9459df
ali2236 <ali.gh2236@gmail.com>
2025-11-13 11:49:28 +0330

    Fix: Resolve TooltipPositionDelegate ambiguity
    The import for `package:flutter/material.dart` is modified to explicitly hide `TooltipPositionDelegate`. This resolves a name collision with a local definition of the same class.

--- lib/src/super_tooltip.dart
+++ lib/src/super_tooltip.dart
@@ -2,7 +2,7 @@
 
 import 'dart:ui';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide TooltipPositionDelegate;
 import 'package:super_tooltip/src/utils.dart';
 
 import 'bubble_shape.dart';
